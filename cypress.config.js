cb7afa49946203fa29d8257e3d33a8b286a0e3ab
Carias <diegocarias789@gmail.com>
2025-03-20 22:49:31 -0600

    Configure path

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    specPattern: 'cypress/support/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/**/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5000',
     supportFile: false
   },
